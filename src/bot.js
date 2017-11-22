0d0c58dcdf51eddfe9a4486b7f4863557263bab8
spences10 <spences10apps@gmail.com>
2017-11-22 13:55:48 +0000

    set up basic object array

--- src/bot.js
+++ src/bot.js
@@ -5,13 +5,24 @@ const bot = new Twit(config)
 
 // use stream to track keywords
 const trackStream = bot.stream('statuses/filter', {
-  track: 'trump'
+  track: 'javascript'
 })
 
 trackStream.on('tweet', log)
 
+let thing = []
+
+// add tweet to object
 function log ( e ) {
+  // thing.push(e.text)
+  
   console.log('====================')
-  console.log(e.text)
+  thing.push({
+    tweet: e.text,
+    timeIn: Date(),
+    timeOut: `Some other Date`
+  })
+  console.log(thing)
   console.log('====================')
+
 }
