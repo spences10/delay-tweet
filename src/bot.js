546a36e89dbed40827c7bce2b1d26b45527e18b2
spences10 <spences10apps@gmail.com>
2017-11-22 07:24:14 +0000

    simple log on stream

--- src/bot.js
+++ src/bot.js
@@ -2,3 +2,16 @@ const Twit = require('twit')
 const config = require('./config')
 
 const bot = new Twit(config)
+
+// use stream to track keywords
+const trackStream = bot.stream('statuses/filter', {
+  track: 'trump'
+})
+
+trackStream.on('tweet', log)
+
+function log ( e ) {
+  console.log('====================')
+  console.log(e.text)
+  console.log('====================')
+}
