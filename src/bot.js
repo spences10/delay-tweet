cd18b3e0571b0c57395cf97b356688485ef9e5d8
spences10 <spences10apps@gmail.com>
2017-11-23 06:20:23 +0000

    attempt loop the keys

--- src/bot.js
+++ src/bot.js
@@ -15,6 +15,7 @@ trackStream.on('tweet', log)
 let tweets = []
 
 // add tweet to object
+// e is the tweet event
 function log(e) {
   console.log('====================')
   tweets.push({
@@ -22,19 +23,37 @@ function log(e) {
     timeIn: newTimeIn(),
     timeOut: newTimeOut()
   })
-  console.log(tweets)
+  // console.log(tweets)
   console.log('====================')
 }
 
-// function to rerurn random 
+// function to rerurn random
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const newTimeIn = (date) => {
+// set timeIn
+const newTimeIn = date => {
   return moment(date).toDate()
 }
 
-const newTimeOut = (date) => {
-  return moment(date).add(30, 'm').toDate()  
+// set timeOut 🙃
+const newTimeOut = date => {
+  return moment(date)
+    .add(getRandomInt(1, 30), 'm')
+    .toDate()
 }
+
+setInterval(function(tweets) {
+  Object.keys(tweets).forEach(key => {
+    console.log(key, tweets[key])
+  })
+}, 3000)
+
+// loop through tweets object
+// popp off tweets after time out is matched
+// function tweetList(tweets) {
+//   Object.keys(tweets).forEach(key => {
+//     console.log(key, tweets[key])
+//   })
+// }
