8edf6e788528fbb5de5b3e183fef55c2af97c222
spences10 <spences10apps@gmail.com>
2017-11-24 13:37:06 +0000

    add retweet

---
+++ src/api/retweet.js
@@ -0,0 +1,21 @@
+const Twit = require('twit')
+const config = require('../config')
+
+const bot = new Twit(config)
+
+const retweet = (event) => {
+  // console.log(JSON.stringify(event.lang))
+  // console.log(JSON.stringify(event))
+  // event.source.screen_name
+  console.log('====================')
+  console.log('RETWEET EVENT: ', event)
+  console.log('====================')
+  bot.post('statuses/retweet/:id', { id: event.id_str }, (err, res) => {
+    if (err) {
+      console.log('RETWEET ERRORDERP: ', err.message)
+    }
+    console.log('RT SUCCESS: ', event.text)
+  })
+}
+
+module.exports = retweet
